6e7cf8704c381e99be7e4941ca08ac40ad6d8775
mimimeowmeo <110224309+mimimeowmeo@users.noreply.github.com>
2024-04-25 21:33:59 +0800

    json

--- app/api/project/[id]/route.ts
+++ app/api/project/[id]/route.ts
@@ -1,10 +1,23 @@
-import { projectList } from "@/data/mock";
+import { TextImageTemplateProps } from "@/components/template/TextImageTemplate";
 import { NextResponse } from "next/server";
 
 type Params = { id: string };
 
+type Project = {
+  id: string;
+  bannerUrl?: string;
+  title: string;
+  description?: string;
+  previewImage: string;
+  urlName: string;
+  year?: string | [string, string];
+  article: TextImageTemplateProps[];
+};
+type ProjectData = { data: Project[] };
+const projectList: ProjectData = require("@/data/mock");
+
 export async function GET(request: Request, context: { params: Params }) {
-  const data = projectList.find(
+  const data = projectList.data.find(
     (project) => project.urlName === context.params.id
   );
   return NextResponse.json(
--- app/api/project/route.ts
+++ app/api/project/route.ts
@@ -1,11 +1,19 @@
-import { projectList } from "@/data/mock";
+import { TextImageTemplateProps } from "@/components/template/TextImageTemplate";
 import { NextResponse } from "next/server";
 
+type Project = {
+  id: string;
+  bannerUrl?: string;
+  title: string;
+  description?: string;
+  previewImage: string;
+  urlName: string;
+  year?: string | [string, string];
+  article: TextImageTemplateProps[];
+};
+type ProjectData = { data: Project[] };
+const projectList: ProjectData = require("@/data/mock");
+
 export async function GET() {
-  return NextResponse.json(
-    {
-      data: projectList,
-    },
-    { status: 200 }
-  );
+  return NextResponse.json(projectList, { status: 200 });
 }
